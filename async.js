5b1e678338372badbe4b9366a9efc979cc8cad5e
Swastika Satya <swastika22102@iiitnr.edu.in>
2025-04-12 18:00:58 +0530

    modifying jsfiles

--- async.js
+++ async.js
@@ -1,4 +1,6 @@
 async function kitchen(){
     await TimeRanges();
     console.log("hey this is just me testing js")
+    await TimeChanges();
+    console.log("het i was changing fe more things so here it is")
 }
